95e11e1deb1bbd7bf20b6b790b0db03a0ff2caf7
Daniel Souza <me@posix.dev.br>
2022-05-09 13:59:27 -0300

    Implement web server w/ Express

---
+++ serve.js
@@ -0,0 +1,8 @@
+// https://expressjs.com/en
+const express = require("express")
+const app = express()
+const http = require("http").Server(app)
+const port = 4000
+
+app.use(express.static("./test"))
+http.listen(port, () => console.log(`Listening on port ${port}`))
